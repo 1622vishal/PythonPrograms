01b529ab9642d99cbdbcea7a97b40a9d3bf6316b
Vishal Yadav <vishalyadav82738@gmail.com>
2023-06-29 13:33:01 +0530

    First program in python

---
+++ FirstProgram.py
@@ -0,0 +1,16 @@
+#----Written by :- Vishal Yadav----#
+#----Date :- 24-06-23----#
+
+# Program to demonstrate how the print function works
+
+print("Hello World!!!")       #Whatever you want to print must be enclosed within double quotes
+print(10)         #We can also print numbers directly
+print("Hello World" , 108)       #We can print multiple things separated by comma
+print(12*3)       #We can perform any type of calculations(See output)
+print("If you want to break line \n use backslash n")
+print("To print double quotes (\") use backslash and to print backslash (\\) we use escape sequences")
+"""These are called escape sequences which are printed on the output screen """
+
+#This is a single line comment
+"""Comments in triple double quotes are multiline comments"""
+'''Comments in triple single quotes are also multiline comments'''
