984ad6fe7230d44879ed6ff080f10608421a2951
Vishal Yadav <vishalyadav82738@gmail.com>
2023-07-10 12:04:03 +0530

    Added Operators.py

---
+++ Operators.py
@@ -0,0 +1,23 @@
+#----Written by :- Vishal Yadav----#
+#----Date :- 30-06-23----# 
+
+"""Program to demonstrate the use of operators and how to make a calculator"""
+
+a = 20
+b = 8
+print("The addition of two no. is", a+b)            #Performs addition
+print("The subtraction of two no. is", a-b)         #Performs subtraction
+print("The multiplication of two no. is", a*b)      #Performs multiplication
+print("The division of two no. is", a/b)            #Performs division(answer in float or actual answer)
+print("The division of two no. is", a//b)           #Performs division(amswer in int)
+print("The remainder of two no. is", a%b)           #Gives remainder
+print("The exponent of two no. is", a**b)           #Gives exponent of the number(20^8)
+
+"""Some more operations"""
+
+#It gives answer in boolean i.e., True or False
+print("Is a>b?", a>b)       
+print("Is a<b?", a<b)
+print("Is a=b?", a==b)
+
+"""Similarly we can also perform >= and <="""
